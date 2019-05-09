c2429e36860b496e8b7e32a0310bef3f0e04da96
lambda x <lambda-162@users.noreply.owo.codes>
2019-05-09 14:48:51 +0000

    schema: sql syntax fixes

--- schema.sql
+++ schema.sql
@@ -5,7 +5,6 @@ CREATE TABLE IF NOT EXISTS pages(
 	title VARCHAR(200) NOT NULL,
 	-- lets us find the text of the page
 	latest_revision INTEGER NOT NULL,
-	-- whether to restrict editing this page to moderators
 	guild BIGINT NOT NULL,
 	-- this information could be gotten by just looking at the date of the oldest revision
 	-- but this way is easier
@@ -17,21 +16,21 @@ CREATE INDEX IF NOT EXISTS pages_name_trgm_idx ON pages USING GIN (title gin_trg
 CREATE TABLE IF NOT EXISTS revisions(
 	revision_id SERIAL PRIMARY KEY,
 	-- what page is this a revision of?
-	page_id INTEGER REFERENCES pages NOT NULL ON DELETE CASCADE,
+	page_id INTEGER NOT NULL REFERENCES pages ON DELETE CASCADE,
 	-- the user ID who created this revision
 	author BIGINT NOT NULL,
 	content VARCHAR(2000) NOT NULL,
 	revised TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP);
 
 CREATE TABLE IF NOT EXISTS role_permissions(
-	role BIGINT PRIMARY KEY
+	role BIGINT PRIMARY KEY,
 	-- not strictly necessary but it lets us delete by guild
 	guild BIGINT NOT NULL,
 	permissions BIGINT NOT NULL);
 
 CREATE TABLE IF NOT EXISTS page_permissions(
-	page_id INTEGER REFERENCES pages NOT NULL ON DELETE CASCADE,
-	role BIGINT REFERENCES role_permissions NOT NULL ON DELETE CASCADE,
+	page_id INTEGER NOT NULL REFERENCES pages ON DELETE CASCADE,
+	role BIGINT NOT NULL REFERENCES role_permissions ON DELETE CASCADE,
 	-- permissions to allow which overwrite role permissions
 	allow BIGINT NOT NULL,
 	-- permissions to deny
