c68d95e8c825e12f652ed7f8eee33ae2037161d9
lambda x <lambda-162@users.noreply.owo.codes>
2019-05-09 00:56:37 +0000

    schema: remove old permissions shit

--- schema.sql
+++ schema.sql
@@ -22,26 +22,3 @@ CREATE TABLE IF NOT EXISTS revisions(
 	author BIGINT NOT NULL,
 	content VARCHAR(2000) NOT NULL,
 	revised TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP);
-
-DO $$ BEGIN IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'page_restriction') THEN
-	-- who can perform a certain action on a page
-	-- this is ordered by hierarchy: moderators can do everything verified users can do
-	CREATE TYPE page_restriction AS ENUM ('verified_users', 'moderators');
-	-- what action are these people allowed to perform?
-	-- for instance, to delete a page, deny view access to everyone but moderators
-	CREATE TYPE page_restriction_level AS ENUM ('view', 'edit', 'delete'); END IF; END; $$;
-
-CREATE TABLE IF NOT EXISTS page_restrictions(
-	pr_id SERIAL PRIMARY KEY,
-	page_id INTEGER REFERENCES pages NOT NULL,
-	pr_type page_restriction NOT NULL,
-	pr_level page_restriction_level NOT NULL);
-
-CREATE UNIQUE INDEX IF NOT EXISTS page_restrictions_uniq_idx ON page_restrictions(page_id, pr_type, pr_level);
-
-CREATE TABLE IF NOT EXISTS guild_settings(
-	guild BIGINT PRIMARY KEY,
-	moderator_role BIGINT,
-	verified_role BIGINT,
-
-	CHECK (moderator_role IS NOT NULL OR verified_role IS NOT NULL));
