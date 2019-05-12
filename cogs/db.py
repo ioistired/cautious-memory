80e8cd79d2b247f6fd561f8a847336be8fd978a5
lambda x <lambda-162@users.noreply.owo.codes>
2019-05-12 20:21:51 +0000

    cogs/db.permissions_for: fix operator precedence error
    x | y & ~z is equivalent to x | (y & ~z), not (x | y) & ~z
    To consider why the former is wrong, consider x, y, z = 1, 0, 1
    0 & ~1 = 0, 1 | 0 = 0
    but z = denied permissions, so the denied permissions had no effect

--- cogs/db.py
+++ cogs/db.py
@@ -228,7 +228,7 @@ class Database(commands.Cog):
 		roles = list(map(operator.attrgetter('id'), member.roles)) + [member.guild.id]
 		perms = await self.bot.pool.fetchval("""
 			WITH page_id AS (SELECT page_id FROM pages WHERE guild = $1 AND LOWER(title) = LOWER($2))
-			SELECT bit_or(permissions) | COALESCE(bit_or(allow), 0) & ~COALESCE(bit_or(deny), 0)
+			SELECT (bit_or(permissions) | COALESCE(bit_or(allow), 0)) & ~COALESCE(bit_or(deny), 0)
 			FROM role_permissions LEFT JOIN page_permissions USING (role)
 			WHERE
 				role = ANY ($3)
